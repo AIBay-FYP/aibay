dc1d8628bab6296da74ac8a1c896088e241ad918
abduvert <sajjadabdullah345@gmail.com>
2025-03-11 09:24:19 +0500

    Phone number auth

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.aibay"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "26.3.11579264"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
